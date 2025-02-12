58352550d174c983db645989cbac8dc5f6bfe192
Subhojeet <subhojeetdas2016@gmail.com>
2025-02-12 22:00:06 +0530

    Fixed case issue in import

--- frontend/src/components/admin/Applicants.jsx
+++ frontend/src/components/admin/Applicants.jsx
@@ -1,47 +1,50 @@
-import React, { useEffect } from 'react';
-import Navbar from '../shared/Navbar';
-import ApplicantsTable from './ApplicantsTable';
-import axios from 'axios';
-import { APPLICATION_API_END_POINT } from '@/utils/constant';
-import { useParams } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { setAllApplicants } from '@/redux/applicationSlice';
+import React, { useEffect } from "react";
+import Navbar from "../shared/navbar";
+import ApplicantsTable from "./ApplicantsTable";
+import axios from "axios";
+import { APPLICATION_API_END_POINT } from "@/utils/constant";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setAllApplicants } from "@/redux/applicationSlice";
 
 const Applicants = () => {
-    const params = useParams();
-    const dispatch = useDispatch();
-    const { applicants } = useSelector(store => store.application);
+  const params = useParams();
+  const dispatch = useDispatch();
+  const { applicants } = useSelector((store) => store.application);
 
-    console.log("Params from useParams:", params);
-    console.log("Applicants from Redux store:", applicants);
+  console.log("Params from useParams:", params);
+  console.log("Applicants from Redux store:", applicants);
 
-    useEffect(() => {
-        const fetchAllApplicants = async () => {
-            try {
-                console.log("Fetching applicants for job ID:", params.id);
-                const res = await axios.get(`${APPLICATION_API_END_POINT}/${params.id}/applicants`, { withCredentials: true });
-                console.log("Response from API:", res.data);
+  useEffect(() => {
+    const fetchAllApplicants = async () => {
+      try {
+        console.log("Fetching applicants for job ID:", params.id);
+        const res = await axios.get(
+          `${APPLICATION_API_END_POINT}/${params.id}/applicants`,
+          { withCredentials: true }
+        );
+        console.log("Response from API:", res.data);
 
-                dispatch(setAllApplicants(res.data.job));
-                console.log("Dispatched applicants to Redux store.");
-            } catch (error) {
-                console.error("Error fetching applicants:", error);
-            }
-        };
-        fetchAllApplicants();
-    }, [params.id, dispatch]);
+        dispatch(setAllApplicants(res.data.job));
+        console.log("Dispatched applicants to Redux store.");
+      } catch (error) {
+        console.error("Error fetching applicants:", error);
+      }
+    };
+    fetchAllApplicants();
+  }, [params.id, dispatch]);
 
-    return (
-        <div>
-            <Navbar />
-            <div className='max-w-7xl mx-auto'>
-                <h1 className='font-bold text-xl my-5'>
-                    Applicants {applicants?.applications?.length}
-                </h1>
-                <ApplicantsTable />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <div className="max-w-7xl mx-auto">
+        <h1 className="font-bold text-xl my-5">
+          Applicants {applicants?.applications?.length}
+        </h1>
+        <ApplicantsTable />
+      </div>
+    </div>
+  );
 };
 
 export default Applicants;
