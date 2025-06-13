1380d47a478a4d347e0e4bd36aecc196f361380e
Subhojeet <subhojeetdas2016@gmail.com>
2025-06-13 14:24:23 +0530

    mongo fixed

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,15 +1,12 @@
 import mongoose from "mongoose";
-const uri = 'mongodb+srv://subhojeetdas2016:i58ezkeGLLGu0oVi@cluster0.f5uyx.mongodb.net/test?retryWrites=true&w=majority'
-PORT = 3000
+
 const connectDb = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log("mongo connected successfully");
+        console.log("MongoDB connected successfully");
+    } catch (err) {
+        console.error("MongoDB connection error:", err);
     }
-    catch (err) {
-        console.error(err);
-    }
-}
-
+};
 
 export default connectDb;
