a2610165a3ece60dac9cdfb93d32467606070893
Subhojeet <subhojeetdas2016@gmail.com>
2025-06-13 14:15:51 +0530

    mongo fixed

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-const uri = 'mongodb+srv://subhojeetdas2016:i58ezkeGLLGu0oVi@cluster0.f5uyx.mongodb.net/'
+const uri = 'mongodb+srv://subhojeetdas2016:i58ezkeGLLGu0oVi@cluster0.f5uyx.mongodb.net/test?retryWrites=true&w=majority'
+PORT = 3000
 const connectDb = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
